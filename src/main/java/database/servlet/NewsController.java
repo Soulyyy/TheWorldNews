65a660075ab42f9ce8ab8c02e9f71375ab5fc1c3
SilverT <silvertiik7@gmail.com>
2014-03-21 03:49:49 +0200

    jama

--- src/main/java/database/servlet/NewsController.java
+++ src/main/java/database/servlet/NewsController.java
@@ -1,5 +1,27 @@
 package database.servlet;
 
-public class NewsController {
+public class NewsController extends HttpServlet {
 
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	       
+        String image = request.getParameter("image");//not really image?
+        String header = request.getParameter("header");
+        String content = request.getParameter("content");
+        //Assign prime number for each article group, multiply if in more than one, unique factorization defines
+        int articleGroups = request.getParameter("articleGroup");
+		
+		
+		//Need to implement all methods in interface
+		String idString = req.getParameter("id");
+		if (idString != null) {
+			List<User> allUsers= datastore.findAllUsers();
+			resp.getWriter().write(gson.toJson(allUsers));
+		} else {
+			int id = Integer.parseInt(idString);
+	        User user = datastore.findUserById(id);
+	        resp.getWriter().write(gson.toJson(user));
+		}
+	}
 }
