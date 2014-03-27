83fbe353c8f83d4c167e982396b63e4ba4e857df
HansP <soulyyy@gmail.com>
2014-03-28 00:23:19 +0200

    push, silver proovi

--- src/main/webapp/js/registerUser.js
+++ src/main/webapp/js/registerUser.js
@@ -24,7 +24,7 @@ $(this).ready(function() {
                 }
                 else {
                 console.log(JSON.stringify(userdata));
-                $.ajax('/accountSignup',{
+                $.ajax("/accountSignup",{
                   
                     type:"POST",
   
