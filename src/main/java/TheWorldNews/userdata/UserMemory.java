776aa7c3350621882c7ef21eb54a05617459a6b6
HansP <soulyyy@gmail.com>
2014-03-28 02:55:13 +0200

    fail?

--- src/main/java/TheWorldNews/userdata/UserMemory.java
+++ src/main/java/TheWorldNews/userdata/UserMemory.java
@@ -17,13 +17,13 @@ import TheWorldNews.externalFunctions.*;
  */
 
 public class UserMemory implements UserDataProvider{
-	
+	@Override
 	public UserMemory(){
 		ArrayList<User> users = findAllUsers();
 	}
 	
 	
-
+	@Override
 	public User findUserById(int id) {
 		String query = "SELECT * FROM users WHERE id ="+id;
 		ArrayList<User> allUsers= DatabaseCommands.getUserQuery(query);
@@ -32,25 +32,25 @@ public class UserMemory implements UserDataProvider{
 	}
 
 
-
+	@Override
 	public void addUser(User user) {
 		DatabaseCommands.addUser(user);
 		
 	}
 
-
+	@Override
 	public ArrayList<User> findAllUsers() {
 		ArrayList<User> allUsers= DatabaseCommands.getUserQuery("SELECT * FROM users");
 		return allUsers;
 	}
 
-
+	@Override
 	public List<User> findAllUsersByGroup(int groupId) {
 		String query = "SELECT * FROM users WHERE accessrights ="+groupId;
 		ArrayList<User> allUsers= DatabaseCommands.getUserQuery(query);
 		return (allUsers);
 	}
-
+	@Override
 	public List<User> findAllUsersByCountry(int country) {
 		String query = "SELECT * FROM users WHERE country ="+country;
 		ArrayList<User> allUsers= DatabaseCommands.getUserQuery(query);
