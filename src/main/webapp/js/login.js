e81d7d3e187c495ad0a253a8457b3af970d54a0d
silverm6 <silvermtkv@gmail.com>
2014-04-02 23:34:09 +0300

    l

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,14 +1,37 @@
 $(this).ready(function() {
- 
+	 var loadpage = function(dest)  {
+		var id=3;
+
+		$.ajax({
+			type:"GET",
+			url:'./html/'+ dest +".html",
+			data:{"id":id},
+			
+			crossDomain:true,
+			success: function(data) {
+				window.location.hash = dest;
+				var externalHTML = document.getElementById("articleGroup");
+
+				externalHTML.innerHTML=data;
+			}
+
+		});    
+	};
+
      
-    $('#log').click(function() {
+    $('#login').click(function() {
+		var hash = window.location.hash;
+		console.log(hash);
+		if (hash) {
+			hash = hash.substr(1);
+		}
         var userdata = new Object();
         userdata.userName = $("#login").val();
         userdata.password = $("#password").val();
  
  
         if (!userdata.userName || !userdata.password  ) {
-            alert("Fill all forms.");
+            alert("Fill all forms.idk this should only popup when clicking on login but it overlaps with register vms");
 		}
 		else {
 			$.ajax("/accountLogin",{
@@ -20,6 +43,7 @@ $(this).ready(function() {
 				success: function(userdata){   
 			 
 					console.log("gg");
+					loadpage(hash);
 					 
 				},
 				error:function(req, text) {
