c6e1ad5a09fb839c62d3ed815f5264f0a7ba170a
silverm6 <silvermtkv@gmail.com>
2014-04-03 23:14:12 +0300

    n

--- src/main/webapp/js/mainPage.js
+++ src/main/webapp/js/mainPage.js
@@ -44,78 +44,7 @@ $(this).ready(function(){
 		}
 	
 	});
-	$('#regi').click( function() {
-		var destination = 'registerUser';
-		var id=3;
-		var authorizeButton = document.getElementById('authorize-button');
-		var container = document.getElementById('loginContainer');
-		var reg = document.getElementById('regi');
-		authorizeButton.style.visibility = 'hidden';
-		regi.style.visibility = 'hidden';
-		container.style.visibility = 'hidden';
-		$.ajax({
-			type:"GET",
-			url:'./html/'+ destination +".html",
-			data:{"id":id},
-			
-			crossDomain:true,
-			success: function(data) {
-				window.location.hash = destination;
-				var externalHTML = document.getElementById("articleGroup");
-
-				externalHTML.innerHTML=data;
-				$('#reg').click(function() {
-					var userdata = new Object();
-					userdata.userName = $("#username").val();
-					userdata.password = $("#pw").val();
-					var pw2 = $("#pw2").val();  
-					userdata.firstname = $("#first").val();
-					userdata.surname = $("#last").val();
-					userdata.email = $("#email").val();
-			 
-					if (!userdata.userName || !userdata.password || !pw2 || !userdata.firstname || !userdata.surname  || !userdata.email) {
-						alert("T2ida k6ik vormid.");
-					}
-					else {
-						if ( userdata.password != pw2 ) {
-							alert("Paroolid peavad olema samad.");
-						}
-						else {
-							if (pw2.length <1){
-								alert("Parooli pikkus peab olema v2hemalt 5.");
-							 
-							}
-							else {
-							console.log(JSON.stringify(userdata));
-							$.ajax("/accountSignup",{
-							  
-								type:"POST",
-								dataType:'json',  
-								data: JSON.stringify(userdata),
-								contentType: 'application/json',
-
-								success: function(userdata){   
-								
-									console.log("gg");
-									window.location.href = "../index.html";
-									 
-								},
-								error:function(req, text) {
-									console.log(req);
-									console.log(text);
-								}
-							   
-							});
-							}
-						}
-					}
-					 
-				});
-			}
-
-		});    
 	 
-	});
 });
 var loadpage = function(dest)  {
     var id=3;
