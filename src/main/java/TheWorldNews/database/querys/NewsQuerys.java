e91f883ac8ec61a1c0151b243d4afa84c1c3d080
HansP <soulyyy@gmail.com>
2014-04-04 03:24:40 +0300

    Is this really a broblem? Bro, pun intended.

--- src/main/java/TheWorldNews/database/querys/NewsQuerys.java
+++ src/main/java/TheWorldNews/database/querys/NewsQuerys.java
@@ -1,7 +1,9 @@
 package TheWorldNews.database.querys;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,6 +16,33 @@ import TheWorldNews.newsdata.NewsArticle;
 
 
 public class NewsQuerys {
+	
+	//Public connection for all database querys
+		public static Connection getConnection() throws SQLException, URISyntaxException {
+	        String databaseUrl = System.getenv("DATABASE_URL");
+	        if (databaseUrl != null) {
+	            return getHerokuConnection(new URI(databaseUrl));
+	        } else {
+	            return getLocalConnection();
+	        }
+	    }
+		
+		
+		static Connection getHerokuConnection(final URI dbUri)throws SQLException, URISyntaxException {
+			System.out.println("We are trying to connect with Heroku.");
+			String username = dbUri.getUserInfo().split(":")[0];
+			String password = dbUri.getUserInfo().split(":")[1];
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost()
+						+ ':' + dbUri.getPort() + dbUri.getPath();
+		 
+			return DriverManager.getConnection(dbUrl, username, password);
+		    } 
+		static Connection getLocalConnection() throws SQLException {
+			return DriverManager.getConnection(
+					"jdbc:postgresql://localhost/mydb", "user", "pass");
+			}
+	
+	
 	
 	//Other querys
 	
