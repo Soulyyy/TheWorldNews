a881bd5bc0a1babc87e5f56bb0d62090504fa26c
HansP <soulyyy@gmail.com>
2014-04-07 21:28:13 +0300

    does this work better

--- src/main/java/TheWorldNews/database/querys/LoginQueries.java
+++ src/main/java/TheWorldNews/database/querys/LoginQueries.java
@@ -25,7 +25,7 @@ public class LoginQueries {
 	public static boolean loginVerification(String username, String password) throws SQLException, URISyntaxException {
 		System.out.println("Getting if user can log in");
 		Connection con = DatabaseConnection.getConnection();
-		String query = "SELECT accessrights from users where username="+username+" AND password="+password;
+		String query = "SELECT accessrights from users where username = ? AND password = ?";
 		PreparedStatement pst = con.prepareStatement(query);
 		//If resultset is empty, not valid login details
 		return(pst.executeQuery().next());
@@ -34,8 +34,10 @@ public class LoginQueries {
 	public static int loginWithAccessrights(String username, String password) throws SQLException, URISyntaxException {
 		System.out.println("Access rights with verifictation, good login");
 		Connection con = DatabaseConnection.getConnection();
-		String query = "SELECT accessrights from users where username="+username+" AND password="+password;
+		String query = "SELECT accessrights from users where username = ? AND password = ?";
 		PreparedStatement pst = con.prepareStatement(query);
+		pst.setString(1, username);
+		pst.setString(2, password);
 		if(!(pst.executeQuery().next())) return -1;
 		else {
 			ResultSet rs = pst.executeQuery();
