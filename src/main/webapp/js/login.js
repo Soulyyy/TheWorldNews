1ae07f63a598eb364b4ad304d182ea1147629d42
silverm6 <silvermtkv@gmail.com>
2014-04-10 23:27:09 +0300

    minor

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -17,7 +17,30 @@ $(this).ready(function() {
 
 		});    
 	};
+	function createCookie(name,value,days) {
+		if (days) {
+			var date = new Date();
+			date.setTime(date.getTime()+(days*24*60*60*1000));
+			var expires = "; expires="+date.toGMTString();
+		}
+		else var expires = "";
+		document.cookie = name+"="+value+expires+"; path=/";
+	}
 
+	function readCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	}
+
+	function eraseCookie(name) {
+		createCookie(name,"",-1);
+	}
      
     $('#loginbutton').click(function() {
 		var hash = window.location.hash;
@@ -44,7 +67,10 @@ $(this).ready(function() {
 					if (userdata.response ==-1) {
 						alert("Vale parool/user.");
 					}
-					console.log(userdata.response);
+					else {
+						createCookie(userdata.UserName,userdata.response,7);
+					}
+					//console.log(userdata.response);
 					 
 				},
 				error:function(req, text) {
