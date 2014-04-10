88c7a733afdcd56892abe5c5d2ea602987cae038
silverm6 <silvermtkv@gmail.com>
2014-04-11 00:10:23 +0300

    minor

--- src/main/webapp/js/mainPage.js
+++ src/main/webapp/js/mainPage.js
@@ -64,12 +64,12 @@ function createCookie(name,value,days) {
 	document.cookie = name+"="+value+expires;
 }
 function readCookie(name) {
-	var nameEQ = name + "=";
+	var cname = name + "=";
 	var ca = document.cookie.split(';');
 	for(var i=0;i < ca.length;i++) {
 		var c = ca[i];
 		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		if (c.indexOf(cname) == 0) return c.substring(cname.length,c.length);
 	}
 	return "";
 }
