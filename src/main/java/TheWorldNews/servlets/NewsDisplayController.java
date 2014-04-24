0c8a33177574bca97b4eb5e080c4ae6f12793657
SilverT <silvertiik7@gmail.com>
2014-04-25 00:04:24 +0300

    vol2

--- src/main/java/TheWorldNews/servlets/NewsDisplayController.java
+++ src/main/java/TheWorldNews/servlets/NewsDisplayController.java
@@ -21,6 +21,7 @@ public class NewsDisplayController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
 
+	
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
