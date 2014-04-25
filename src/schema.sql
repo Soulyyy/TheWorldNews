be1b9c82cb1ecbae27988a5b5ee2fca114a6b9b1
HansP <soulyyy@gmail.com>
2014-04-25 06:17:24 +0300

    ts

--- src/schema.sql
+++ src/schema.sql
@@ -7,7 +7,7 @@ accessrights integer NOT NULL, country integer NOT NULL);
 
 
 CREATE TABLE IF NOT EXISTS newsarticles(id SERIAL NOT NULL PRIMARY KEY, image varchar(225),
-					 header TEXT NOT NULL, content TEXT NOT NULL, articlegroup integer NOT NULL);
+					 header TEXT NOT NULL, content TEXT NOT NULL, articlegroup integer NOT NULL, author varchar(225) NOT NULL, clickcount integer);
 
 					 
 CREATE TABLE IF NOT EXISTS tags(id SERIAL NOT NULL PRIMARY KEY, tagname VARCHAR(225) NOT NULL UNIQUE, tagcount INTEGER NOT NULL);
