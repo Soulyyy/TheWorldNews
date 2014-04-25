f729366f845eab752fb5a6510311ea11de67319c
HansP <soulyyy@gmail.com>
2014-04-25 04:15:32 +0300

    Changes to object, prints to verify

--- src/main/java/TheWorldNews/filters/EditrightCheckFilter.java
+++ src/main/java/TheWorldNews/filters/EditrightCheckFilter.java
@@ -26,7 +26,7 @@ public class EditrightCheckFilter implements Filter {
 	    HttpServletResponse res = (HttpServletResponse) response;  
 
 	    if (req.getSession().getAttribute("LOGIN_USER") == null) { //checks if there's a LOGIN_USER set in session...
-	        res.sendRedirect(contextPath + "/index.jsp"); //or page where you want to redirect
+	        res.sendRedirect(contextPath + "/Index.jsp"); //or page where you want to redirect
 	    } else {
 	      Integer userType = (Integer)req.getSession().getAttribute("LOGIN_USER");
 	      if (userType < 2){ //check if user type is not admin
--- src/main/java/TheWorldNews/servlets/SubmitNewsController.java
+++ src/main/java/TheWorldNews/servlets/SubmitNewsController.java
@@ -46,6 +46,8 @@ public class SubmitNewsController extends HttpServlet {
 
 			System.out.println("Entered post for adding article");
             NewsArticle article = gson.fromJson(req.getReader(), NewsArticle.class);
+            System.out.println(article.articlegroup);
+            System.out.println(article.articleInt);
             System.out.println("We manage to get JSON object to the required format");
 			NewsQuerys.addArticle(article);
             resp.setHeader("Content-Type", "application/json");
