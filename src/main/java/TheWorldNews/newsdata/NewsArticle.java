ce6d9e64e7b23855835af0c96c293a6af92d7924
HansP <soulyyy@gmail.com>
2014-04-25 05:18:01 +0300

    method call should be fixed, implementation is bad imo

--- src/main/java/TheWorldNews/newsdata/NewsArticle.java
+++ src/main/java/TheWorldNews/newsdata/NewsArticle.java
@@ -11,7 +11,6 @@ public class NewsArticle {
     public String content;
     //Assign prime number for each article group, multiply if in more than one, unique factorization defines
     public String articlegroupstring;
-    public int articleInt;
 
     public NewsArticle() {
     }
@@ -23,29 +22,20 @@ public class NewsArticle {
         this.header = header;
         this.content = content;
         this.articlegroupstring = articlegroupstring;
-        this.articleInt=NewsEncoding.jointArticleConvertToInt(articlegroupstring);
         
 		
     }
 
 
-    public NewsArticle(int id, String header, String content,  int articleInt) {
+    public NewsArticle(int id, String header, String content,  String articlegroupstring) {
     	this.id = id;
         this.header = header;
         this.content = content;
-        this.articleInt = articleInt;
+        this.articlegroupstring = articlegroupstring;
 
     }
     
-    public NewsArticle(int id,String image, String header, String content, int articleInt) {
-    	this.id = id;
-    	this.image = image;
-        this.header = header;
-        this.content = content;
-        this.articleInt= articleInt;
-        this.articlegroupstring = NewsEncoding.convertArticleEncodingToString(articleInt);
 
-    }
     
     
 
