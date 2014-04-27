55b7adfce241866cbfea263517f160074c251951
HansP <soulyyy@gmail.com>
2014-04-27 22:30:47 +0300

    weird naming convention, NO TO THIS STUFF PLX

--- src/main/java/TheWorldNews/newsdata/NewsArticle.java
+++ src/main/java/TheWorldNews/newsdata/NewsArticle.java
@@ -10,28 +10,28 @@ public class NewsArticle {
     public String header;
     public String content;
     //Assign prime number for each article group, multiply if in more than one, unique factorization defines
-    public String articlegroupstring;
+    public String articlegroup;
 
     public NewsArticle() {
     }
 
 
-    public NewsArticle(int id,String image, String header, String content, String articlegroupstring) {
+    public NewsArticle(int id,String image, String header, String content, String articlegroup) {
     	this.id = id;
         this.image = image;
         this.header = header;
         this.content = content;
-        this.articlegroupstring = articlegroupstring;
+        this.articlegroup = articlegroup;
         
 		
     }
 
 
-    public NewsArticle(int id, String header, String content,  String articlegroupstring) {
+    public NewsArticle(int id, String header, String content,  String articlegroup) {
     	this.id = id;
         this.header = header;
         this.content = content;
-        this.articlegroupstring = articlegroupstring;
+        this.articlegroup = articlegroup;
 
     }
     
