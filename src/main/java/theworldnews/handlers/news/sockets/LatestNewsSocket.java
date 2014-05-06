666e45648ee1c06d63c2797690516fc1f6e5fcc1
silverm6 <silvermtkv@gmail.com>
2014-05-06 21:56:28 +0300

    test

--- src/main/java/theworldnews/handlers/news/sockets/LatestNewsSocket.java
+++ src/main/java/theworldnews/handlers/news/sockets/LatestNewsSocket.java
@@ -1,47 +1,47 @@
-package theworldnews.handlers.news.sockets;
+// package theworldnews.handlers.news.sockets;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-import org.eclipse.jetty.websocket.api.Session;
-import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
-import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
-import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
-import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+// import org.eclipse.jetty.websocket.api.Session;
+// import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+// import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+// import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+// import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
-/**
- * 
- * @author S
- * 
- */
-@WebSocket
-public class LatestNewsSocket {
+// /**
+ // * 
+ // * @author S
+ // * 
+ // */
+// @WebSocket
+// public class LatestNewsSocket {
 
-	private Session session;
-	private final LatestNewsSocketController controller;
+	// private Session session;
+	// private final LatestNewsSocketController controller;
 
-	public LatestNewsSocket(LatestNewsSocketController controller) {
-		this.controller = controller;
-	}
+	// public LatestNewsSocket(LatestNewsSocketController controller) {
+		// this.controller = controller;
+	// }
 
-	public void send(String message) throws IOException {
-		if (session.isOpen()) {
-			session.getRemote().sendString(message, null);
-		}
-	}
+	// public void send(String message) throws IOException {
+		// if (session.isOpen()) {
+			// session.getRemote().sendString(message, null);
+		// }
+	// }
 
-	@OnWebSocketConnect
-	public void onOpen(Session session) {
-		this.session = session;
-		controller.getSockets().add(this);
-	}
+	// @OnWebSocketConnect
+	// public void onOpen(Session session) {
+		// this.session = session;
+		// controller.getSockets().add(this);
+	// }
 
-	@OnWebSocketClose
-	public void onClose(int status, String message) {
-		controller.getSockets().remove(this);
-	}
+	// @OnWebSocketClose
+	// public void onClose(int status, String message) {
+		// controller.getSockets().remove(this);
+	// }
 
-	@OnWebSocketMessage
-	public void onMessage(Session session, String message) throws IOException {
-		System.out.println("received on websocket: " + message);
-	}
-}
+	// @OnWebSocketMessage
+	// public void onMessage(Session session, String message) throws IOException {
+		// System.out.println("received on websocket: " + message);
+	// }
+// }
--- src/main/java/theworldnews/handlers/news/sockets/LatestNewsSocketController.java
+++ src/main/java/theworldnews/handlers/news/sockets/LatestNewsSocketController.java
@@ -1,138 +1,137 @@
-package theworldnews.handlers.news.sockets;
+// package theworldnews.handlers.news.sockets;
 
  
 
-import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.ArrayList;
+// import java.io.IOException;
+// import java.util.List;
+// import java.util.concurrent.CopyOnWriteArrayList;
+// import java.util.ArrayList;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+// import javax.servlet.ServletConfig;
+// import javax.servlet.ServletContext;
+// import javax.servlet.ServletException;
+// import javax.servlet.annotation.WebServlet;
 
-import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
-import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
-import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
-import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
-import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
-
-
+// import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
+// import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
+// import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
+// import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+// import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
 
 
 
 
-import theworldnews.database.news.objects.Article;
 
-import com.google.gson.Gson;
 
-/**
- * 
- * @author S
- * 
- */
-@WebServlet(value = "/feed")
-public class LatestNewsSocketController extends WebSocketServlet implements
-		WebSocketCreator {
+// import theworldnews.database.news.objects.Article;
 
-	private List<LatestNewsSocket> sockets;
-	private ServletContext context;
-	private Gson gson;
-	private Pinger pinger;
+// import com.google.gson.Gson;
 
+// /**
+ // * 
+ // * @author S
+ // * 
+ // */
+// @WebServlet(value = "/feed")
+// public class LatestNewsSocketController extends WebSocketServlet implements
+		// WebSocketCreator {
+
+	// private List<LatestNewsSocket> sockets;
+	// private ServletContext context;
+	// private Gson gson;
 	// private Pinger pinger;
+ 
 	
 	
-	public void loadLatestNews(){
-		if (sockets.size() > 0) {
-			//List<Article> list = whereEverTheFuck.getLatestNews();
-			List<String> list = new ArrayList<String>();
-			list.add("3");
-			for(LatestNewsSocket socket : sockets){
-				try {
-				socket.send(gson.toJson(list));
-				} catch (IOException e) {
-					System.out.println("failed to send data to " + socket);
-				}
-			}
-		}
-	}
+	// public void loadLatestNews(){
+		// if (sockets.size() > 0) {
+			// List<Article> list = whereEverTheFuck.getLatestNews();
+			// List<String> list = new ArrayList<String>();
+			// list.add("3");
+			// for(LatestNewsSocket socket : sockets){
+				// try {
+				// socket.send(gson.toJson(list));
+				// } catch (IOException e) {
+					// System.out.println("failed to send data to " + socket);
+				// }
+			// }
+		// }
+	// }
 
-	public List<LatestNewsSocket> getSockets() {
-		return sockets;
-	}
+	// public List<LatestNewsSocket> getSockets() {
+		// return sockets;
+	// }
 
-	public ServletContext getContext() {
-		return context;
-	}
+	// public ServletContext getContext() {
+		// return context;
+	// }
 	
-	@Override
-	public void init(ServletConfig config) throws ServletException{
-		super.init(config);
-		sockets = new CopyOnWriteArrayList<LatestNewsSocket>();
-		publish(this, context);
-		context = config.getServletContext(); // shared between ALL servlets
-		gson = new com.google.gson.GsonBuilder().registerTypeAdapter(Article.class,
-				new Object()).create();
-		pinger = new Pinger();
-	}
+	// @Override
+	// public void init(ServletConfig config) throws ServletException{
+		// super.init(config);
+		// sockets = new CopyOnWriteArrayList<LatestNewsSocket>();
+		// publish(this, context);
+		// context = config.getServletContext(); // shared between ALL servlets
+		// gson = new com.google.gson.GsonBuilder().registerTypeAdapter(Article.class,
+				// new Object()).create();
+		// pinger = new Pinger();
+	// }
 
-	public Object createWebSocket(ServletUpgradeRequest req,
-			ServletUpgradeResponse resp) {
+	// public Object createWebSocket(ServletUpgradeRequest req,
+			// ServletUpgradeResponse resp) {
 		// TODO Auto-generated method stub
-		return new LatestNewsSocket(this);
-	}
+		// return new LatestNewsSocket(this);
+	// }
 
-	@Override
-	public void configure(WebSocketServletFactory factory) {
+	// @Override
+	// public void configure(WebSocketServletFactory factory) {
 		// TODO Auto-generated method stub
-		factory.setCreator(this);
-	}
+		// factory.setCreator(this);
+	// }
 
-	public void sendMessage(String text) {
-		for (LatestNewsSocket socket : sockets) {
-			try {
-				socket.send(text);
-			} catch (IOException e) {
-				System.out.println("failed to broadcast to " + socket);
-			}
-		}
-	}
+	// public void sendMessage(String text) {
+		// for (LatestNewsSocket socket : sockets) {
+			// try {
+				// socket.send(text);
+			// } catch (IOException e) {
+				// System.out.println("failed to broadcast to " + socket);
+			// }
+		// }
+	// }
 	
-    private static void publish(LatestNewsSocketController controller, ServletContext context) {
+    // private static void publish(LatestNewsSocketController controller, ServletContext context) {
         // see @WebListener and @WebFilter for details about servlet init
-        context.setAttribute(LatestNewsSocketController.class.getName(), controller);
-    }
+        // context.setAttribute(LatestNewsSocketController.class.getName(), controller);
+    // }
     
-	public static LatestNewsSocketController find(ServletContext context) {
-		return (LatestNewsSocketController) context
-				.getAttribute(LatestNewsSocketController.class.getName());
-	}
+	// public static LatestNewsSocketController find(ServletContext context) {
+		// return (LatestNewsSocketController) context
+				// .getAttribute(LatestNewsSocketController.class.getName());
+	// }
 
-	/**
-	 * Class to keep the connection open. Credit to Jaan Janno.
-	 * https://github.com
-	 * /JaanJanno/OnTime/blob/master/app/controllers/chat/ChatSocket.java
-	 */
-	private class Pinger extends Thread {
+	// /**
+	 // * Class to keep the connection open. Credit to Jaan Janno.
+	 // * https://github.com
+	 // * /JaanJanno/OnTime/blob/master/app/controllers/chat/ChatSocket.java
+	 // */
+	// private class Pinger extends Thread {
 
-		public Pinger() {
-			this.start();
-		}
+		// public Pinger() {
+			// this.start();
+		// }
 
-		@Override
-		public synchronized void run() {
-			while (true) {
-				sendMessage("");
-				try {
-					Thread.sleep(30000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+		// @Override
+		// public synchronized void run() {
+			// while (true) {
+				// sendMessage("");
+				// try {
+					// Thread.sleep(30000);
+				// } catch (InterruptedException e) {
+					// e.printStackTrace();
+				// }
+			// }
+		// }
+	// }
 
-}
+// }
