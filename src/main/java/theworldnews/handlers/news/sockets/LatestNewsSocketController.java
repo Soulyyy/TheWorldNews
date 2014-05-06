c7fd00342fae87b27a3d9897343ad0673087abea
silverm6 <silvermtkv@gmail.com>
2014-05-06 22:09:57 +0300

    test

---
+++ src/main/java/theworldnews/handlers/news/sockets/LatestNewsSocketController.java
@@ -0,0 +1,88 @@
+package theworldnews.handlers.news.sockets;
+
+ 
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.ArrayList;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
+import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+
+
+
+
+
+
+import theworldnews.database.news.objects.Article;
+
+import com.google.gson.Gson;
+
+/**
+ * 
+ * @author S
+ * 
+ */
+ 
+@WebServlet(value = "/feed")
+public class LatestNewsSocketController extends WebSocketServlet implements WebSocketCreator {
+
+    private List<LatestNewsSocket> sockets;
+    private ServletContext context;
+
+    public void broadcast(String message) {
+        for (LatestNewsSocket socket : sockets) {
+            try {
+                socket.send(message);
+            } catch (IOException e) {
+                System.out.println("failed to broadcast to " + socket);
+            }
+        }
+    }
+
+    public List<LatestNewsSocket> getSockets() {
+        return sockets;
+    }
+
+    public ServletContext getContext() {
+        return context;
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        sockets = new CopyOnWriteArrayList<>(); // thread-safe impl
+        context = config.getServletContext(); // shared between ALL servlets
+        publish(this, context); // so that other servlets could find us
+    }
+
+    @Override
+    public Object createWebSocket(ServletUpgradeRequest req, ServletUpgradeResponse resp) {
+        return new LatestNewsSocket(this); // socket instance created per client
+    }
+
+    @Override
+    public void configure(WebSocketServletFactory factory) {
+        factory.setCreator(this);
+    }
+
+    private static void publish(LatestNewsSocketController controller, ServletContext context) {
+        // see @WebListener and @WebFilter for details about servlet init
+        context.setAttribute(LatestNewsSocketController.class.getName(), controller);
+    }
+
+    public static LatestNewsSocketController find(ServletContext context) {
+        return (LatestNewsSocketController) context.getAttribute(LatestNewsSocketController.class.getName());
+    }
+
+}
