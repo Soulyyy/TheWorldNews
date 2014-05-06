e56473a80e93d95e12740a8e7e3e421badfc18c3
SilverT <silvertiik7@gmail.com>
2014-05-07 02:18:34 +0300

    NO NO

--- src/main/webapp/js/mainPage.js
+++ src/main/webapp/js/mainPage.js
@@ -39,11 +39,13 @@ function loadpage(dest) {
 	var id = 3;
 
 	$.ajax({
-		type: "GET",
-		url: '../jsp/' + dest + ".jsp",
-		data: {"id": id},
-		crossDomain: true,
-		success: function(data) {
+		type : "GET",
+		url : '../jsp/' + dest + ".jsp",
+		data : {
+			"id" : id
+		},
+		crossDomain : true,
+		success : function(data) {
 			window.location.hash = dest;
 			var externalHTML = document.getElementById("articleGroup");
 
@@ -51,18 +53,26 @@ function loadpage(dest) {
 		}
 	});
 }
- 
+
 function createWebsocket() {
- 
-	var socketAddr = window.location.origin.replace("http", "ws") + "/feed";
-    var websocket = new WebSocket(socketAddr);
-    websocket.onopen = function() { console.log("socket up!"); };
-    websocket.onclose = function() { console.log("socket closed!"); };
- 
-    websocket.onmessage = function(event) {
-		console.log("ws received " + event.data);	
+	if (!window.location.origin) {
+		window.location.origin = window.location.protocol + "//"
+				+ window.location.hostname
+				+ (window.location.port ? ':' + window.location.port : '');
 	}
-}; 
+	var socketAddr = window.location.origin.replace("http", "ws") + "/feed";
+	var websocket = new WebSocket(socketAddr);
+	websocket.onopen = function() {
+		console.log("socket up!");
+	};
+	websocket.onclose = function() {
+		console.log("socket closed!");
+	};
+
+	websocket.onmessage = function(event) {
+		console.log("ws received " + event.data);
+	}
+};
 
 var recentHash = "";
 
@@ -80,58 +90,40 @@ var checkHash = function() {
 	}
 };
 /*
- function handleClientLoad() {
- var apiKey = 'AIzaSyD7HJs0zDCJKqLcLIK5ok5uAAm33cubOGs';
- var clientId = '510213468349-6npga48p58v7rr3s50p0smnp7e6dho5m.apps.googleusercontent.com';
- var scopes = 'https://www.googleapis.com/auth/plus.me';
- 
- if (readCookie("googlein") !== "") {
- gapi.client.setApiKey(apiKey);
- checkAuth();
- }
- }
- 
- function checkAuth() {
- var clientId = '510213468349-6npga48p58v7rr3s50p0smnp7e6dho5m.apps.googleusercontent.com';
- var scopes = 'https://www.googleapis.com/auth/plus.me';
- gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
- }
- 
- function handleAuthResult(authResult) {
- var authorizeButton = document.getElementById('authorize-button');
- var logoutButton = document.getElementById('logoutButton');
- var logint = document.getElementById('toggleLogin');
- var container = document.getElementById('loginContainer');
- 
- if (authResult && !authResult.error) {
- authorizeButton.style.visibility = 'hidden';
- regi.style.visibility = 'hidden';
- logoutButton.style.visibility = 'visible';
- logint.style.visibility = 'hidden';
- container.style.visibility = 'hidden';
- 
- makeApiCall();
- } else {
- authorizeButton.onclick = handleAuthClick;
- }
- }
- 
- function handleAuthClick(event) {
- var clientId = '510213468349-6npga48p58v7rr3s50p0smnp7e6dho5m.apps.googleusercontent.com';
- var scopes = 'https://www.googleapis.com/auth/plus.me';
- gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
- return false;
- }
- 
- function makeApiCall() {
- gapi.client.load('plus', 'v1', function() {
- var request = gapi.client.plus.people.get({
- 'userId': 'me'
- });
- request.execute(function(resp) {
- user = resp;
- console.log(user);
- });
- });
- }
+ * function handleClientLoad() { var apiKey =
+ * 'AIzaSyD7HJs0zDCJKqLcLIK5ok5uAAm33cubOGs'; var clientId =
+ * '510213468349-6npga48p58v7rr3s50p0smnp7e6dho5m.apps.googleusercontent.com';
+ * var scopes = 'https://www.googleapis.com/auth/plus.me';
+ * 
+ * if (readCookie("googlein") !== "") { gapi.client.setApiKey(apiKey);
+ * checkAuth(); } }
+ * 
+ * function checkAuth() { var clientId =
+ * '510213468349-6npga48p58v7rr3s50p0smnp7e6dho5m.apps.googleusercontent.com';
+ * var scopes = 'https://www.googleapis.com/auth/plus.me';
+ * gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true},
+ * handleAuthResult); }
+ * 
+ * function handleAuthResult(authResult) { var authorizeButton =
+ * document.getElementById('authorize-button'); var logoutButton =
+ * document.getElementById('logoutButton'); var logint =
+ * document.getElementById('toggleLogin'); var container =
+ * document.getElementById('loginContainer');
+ * 
+ * if (authResult && !authResult.error) { authorizeButton.style.visibility =
+ * 'hidden'; regi.style.visibility = 'hidden'; logoutButton.style.visibility =
+ * 'visible'; logint.style.visibility = 'hidden'; container.style.visibility =
+ * 'hidden';
+ * 
+ * makeApiCall(); } else { authorizeButton.onclick = handleAuthClick; } }
+ * 
+ * function handleAuthClick(event) { var clientId =
+ * '510213468349-6npga48p58v7rr3s50p0smnp7e6dho5m.apps.googleusercontent.com';
+ * var scopes = 'https://www.googleapis.com/auth/plus.me';
+ * gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false},
+ * handleAuthResult); return false; }
+ * 
+ * function makeApiCall() { gapi.client.load('plus', 'v1', function() { var
+ * request = gapi.client.plus.people.get({ 'userId': 'me' });
+ * request.execute(function(resp) { user = resp; console.log(user); }); }); }
  */
