02206d463f6fa4d3c5a4c608277234cd4b3d0a38
SilverT <silvertiik7@gmail.com>
2014-05-09 04:21:15 +0300

    sdc

--- src/main/webapp/js/websocket.js
+++ src/main/webapp/js/websocket.js
@@ -3,11 +3,11 @@ var websocket = {
     createWebsocket: function() {
         var socketAddr = window.location.origin.replace("http", "ws") + "/feed";
         var websocket = new WebSocket(socketAddr);
-        console.log(socketAddr);
+        console.log(websocket);
         websocket.onopen = function() { console.log("socket up!"); };
         websocket.onclose = function() { console.log("socket closed!"); };
-        websocket.onmessage = function(evt) {
-
+        websocket.onmessage = function(event) {
+            console.log("ws received " + event.data);
         };
     }
 };
