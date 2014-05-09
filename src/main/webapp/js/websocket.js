a9d798f0d23ae69d4a1d1dd512b4408d072060eb
SilverT <silvertiik7@gmail.com>
2014-05-09 06:29:13 +0300

    event

--- src/main/webapp/js/websocket.js
+++ src/main/webapp/js/websocket.js
@@ -4,8 +4,8 @@ var websocket = {
         var socketAddr = window.location.origin.replace("http", "ws") + "/feed";
         var websocket = new WebSocket(socketAddr);
         console.log(websocket);
-        websocket.onopen = function() { console.log("socket up!"); };
-        websocket.onclose = function() { console.log("socket closed!"); };
+        websocket.onopen = function(event) { console.log("socket up!"); };
+        websocket.onclose = function(event) { console.log("socket closed!"); };
         websocket.onmessage = function(event) {
             console.log("ws received " + event.data);
         };
