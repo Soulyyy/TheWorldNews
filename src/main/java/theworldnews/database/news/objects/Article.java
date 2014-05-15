5a76535635f3f2778f5fd9721544ac4e5e0d21a9
HansP <soulyyy@gmail.com>
2014-05-16 02:22:35 +0300

    minor fix, restored code

--- src/main/java/theworldnews/database/news/objects/Article.java
+++ src/main/java/theworldnews/database/news/objects/Article.java
@@ -5,22 +5,32 @@ package theworldnews.database.news.objects;
  */
 public class Article {
 
-	public Integer id; //Primary key
-	public String image; //Image url
-	public String header; //Header text
-	public String content; //Content text
-	public String articlegroup; //String of groups where the article is displayed
-	public int authorid; //user id of the author
+	public Integer id; // Primary key
+	public String image; // Image url
+	public String header; // Header text
+	public String content; // Content text
+	public String articlegroup; // String of groups where the article is
+								// displayed
+	public int authorid; // user id of the author
 
 	/**
-	 * @param id int, serial primary key of the database entry
-	 * @param image String, url of the image
-	 * @param header String, header of the article
-	 * @param content String, content of the article
-	 * @param articlegroup String, list of groups the article belongs to, int in the database
-	 * @param authorid int, id value from the users table, corresponds to the creator of the article
+	 * @param id
+	 *            int, serial primary key of the database entry
+	 * @param image
+	 *            String, url of the image
+	 * @param header
+	 *            String, header of the article
+	 * @param content
+	 *            String, content of the article
+	 * @param articlegroup
+	 *            String, list of groups the article belongs to, int in the
+	 *            database
+	 * @param authorid
+	 *            int, id value from the users table, corresponds to the creator
+	 *            of the article
 	 */
-	public Article(int id, String image, String header, String content,  String articlegroup, int authorid) {
+	public Article(int id, String image, String header, String content,
+			String articlegroup, int authorid) {
 		this.id = id;
 		this.image = image;
 		this.header = header;
@@ -31,18 +41,26 @@ public class Article {
 
 	/**
 	 *
-	 * @param id int, serial primary key of the database entry
-	 * @param image String, url of the image
-	 * @param header String, header of the article
-	 * @param articleGroup String, list of groups the article belongs to, int in the database
-	 * @param authorid int, id value from the users table, corresponds to the creator of the article
+	 * @param id
+	 *            int, serial primary key of the database entry
+	 * @param image
+	 *            String, url of the image
+	 * @param header
+	 *            String, header of the article
+	 * @param articleGroup
+	 *            String, list of groups the article belongs to, int in the
+	 *            database
+	 * @param authorid
+	 *            int, id value from the users table, corresponds to the creator
+	 *            of the article
 	 */
-	public Article(int id, String image, String header, String articleGroup, int authorid) {
-		this.id = id;
-		this.image = image;
-		this.header = header;
-		this.articlegroup = articlegroup;
-		this.authorid = authorid;
+	public Article(int id, String image, String header, String articleGroup,
+			int authorid) {
+		this(id, image, header, null, articleGroup, authorid);
+	}
+
+	public Article(int id, String header, String articleGroup, int authorid) {
+		this(id, null, header, null, articleGroup, authorid);
 	}
 
 }
