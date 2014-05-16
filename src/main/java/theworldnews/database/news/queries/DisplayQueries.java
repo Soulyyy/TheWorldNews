fb461d97ce55188fdaa76812b35f3a74b0633c28
HansP <soulyyy@gmail.com>
2014-05-16 09:33:59 +0300

    g

--- src/main/java/theworldnews/database/news/queries/DisplayQueries.java
+++ src/main/java/theworldnews/database/news/queries/DisplayQueries.java
@@ -27,11 +27,7 @@ public class DisplayQueries {
 		try {
 			String query = "SELECT userinfo.userid, userinfo.firstname, userinfo.surname,newsarticles.image,"
 					+ "newsarticles.header, newsarticles.articlegroup FROM userinfo"
-					+ "INNER JOIN newsarticles ON userinfo.userid=newsarticles.author WHERE userinfo.userid=?";
-			// String query =
-			// "SELECT newsarticles.image, newsarticles.header, newsarticles.content, newsarticles.articlegroup, newsarticles.author,"
-			// +
-			// "newsarticles.authorid, users.firstname , users.lastname FROM newsarticles WHERE id = ?";
+					+ "INNER JOIN newsarticles ON userinfo.userid=newsarticles.author WHERE newsarticle.id=?";
 			PreparedStatement pst = con.prepareStatement(query);
 			pst.setInt(1, id);
 			ResultSet rs = pst.executeQuery();
