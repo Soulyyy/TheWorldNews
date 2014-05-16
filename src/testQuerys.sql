ebca4a1d75b35487c144d61b525301ce3d2f46ba
HansP <soulyyy@gmail.com>
2014-05-16 06:45:12 +0300

    test

--- src/testQuerys.sql
+++ src/testQuerys.sql
@@ -2,7 +2,7 @@
 
 SELECT userinfo.userid, userinfo.firstname, userinfo.surname,newsarticles.image,newsarticles.header,newsarticles.articlegroup,clickcount FROM userinfo
 					INNER JOIN newsarticles ON userinfo.userid=newsarticles.author WHERE
-					articlegroup % 1 =0 ORDER BY id DESC limit 2;
+					articlegroup % 2 =0 ORDER BY id DESC limit 2;
 
 SELECT * from userinfo;
 SELECT userinfo.userid, userinfo.firstname, userinfo.surname,newsarticles.image,
