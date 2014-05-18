37dc8c06b558ea896290a904e85d2b5898a74c29
HansP <soulyyy@gmail.com>
2014-05-18 03:03:12 +0300

    g

--- src/main/java/theworldnews/handlers/news/servlets/DisplayController.java
+++ src/main/java/theworldnews/handlers/news/servlets/DisplayController.java
@@ -27,7 +27,7 @@ public class DisplayController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		String id = req.getParameter("id");
+		String id = req.getParameter("authorid");
 		String img = req.getParameter("image");
 
 		if (id == null) {
