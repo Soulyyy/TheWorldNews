f14cec631243fd099aa7bea67a486adb74e3b19b
SilverT <silvertiik7@gmail.com>
2014-05-19 22:16:44 +0300

    chage

--- src/main/java/theworldnews/handlers/news/servlets/NoscriptSubmitController.java
+++ src/main/java/theworldnews/handlers/news/servlets/NoscriptSubmitController.java
@@ -14,52 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import theworldnews.database.connection.DatabaseConnection;
 import theworldnews.database.news.objects.Article;
 import theworldnews.database.news.queries.EditQueries;
-<<<<<<< HEAD
 
-import com.google.gson.Gson;
-import com.google.gson.JsonParseException;
 
-@WebServlet(value = "/submitArticle")
-public class NoscriptSubmitController extends HttpServlet {
-
-	private static final long serialVersionUID = 1L;
-
-	private Gson gson;
-
-	@Override
-	public void init() throws ServletException {
-		super.init();
-		gson = new Gson();
-	}
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		resp.setHeader("Content-Type", "application/json");
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		try (Connection con = DatabaseConnection.getConnection()) {
-			Article article = gson.fromJson(req.getReader(), Article.class);
-			article.authorid = (Integer) req.getSession().getAttribute(
-					"LOGIN_ID");
-			System.out.println(article.articlegroup);
-
-			EditQueries.addArticle(con, article);
-			resp.setHeader("Content-Type", "application/json");
-			resp.getWriter().write("{\"response\":\"newsarticle created \"}");
-
-		} catch (JsonParseException ex) {
-			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
-		} catch (SQLException | URISyntaxException e) {
-			resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-					e.getMessage());
-=======
-import theworldnews.handlers.news.sockets.*;
-
-import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
 
 @WebServlet(value = "/noscriptSubmitArticle")
@@ -67,12 +23,10 @@ public class NoscriptSubmitController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	private Gson gson;
 
 	@Override
 	public void init() throws ServletException {
 		super.init();
-		gson = new Gson();
 	}
 
 	@Override
