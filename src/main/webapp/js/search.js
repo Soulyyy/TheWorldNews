de1e66116094e81d3931a0908fb921a93b72c206
silverm6 <silvermtkv@gmail.com>
2014-05-21 21:06:04 +0300

    search

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -6,15 +6,16 @@ $(this).ready(function() {
 	});
  
 	$('#searchbtn').click(function() {	
-		var term =   $('#searchBox').val();
+		var term = $('#searchBox').val();
 		console.log(term);
 		 $.ajax("/searchText", {
 			type: "POST",
-			data:{'fragment':term},
+			contentType: "application/json; charset=utf-8",
+			data:{'term':term},
 			success: function(result) {
-				// console.log("suc");
+
 				console.log(result);
-				},
+			},
 			error: function(result) {
 				console.log("fail search");
 			}
