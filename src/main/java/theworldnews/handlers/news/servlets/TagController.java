80aa72c7674032eb8b2e7ae1dae06642c9910be1
silverm6 <silvermtkv@gmail.com>
2014-05-23 00:51:09 +0300

    tags

--- src/main/java/theworldnews/handlers/news/servlets/TagController.java
+++ src/main/java/theworldnews/handlers/news/servlets/TagController.java
@@ -36,7 +36,7 @@ public class TagController extends HttpServlet {
 			// We display five tags
 			System.out.println("BBBBBBBBBBBBBBBBBBBBBB");
 			System.out.println(articleid);
-			Gson gson = new Gson();
+			// Gson gson = new Gson();
 
 			ArrayList<Tag> taglist = HashTagQueries.topHashTagsOnArticle(con,articleid, 5);
 			// String test2 = gson.toJson(taglist);
