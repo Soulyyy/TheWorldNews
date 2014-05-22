891ad38c672f0dfd9a344e1c14f2423710ccc29a
HansP <soulyyy@gmail.com>
2014-05-22 04:08:16 +0300

    Hashtag queries, somewhat tested as well.

---
+++ src/taggedTest.sql
@@ -0,0 +1,7 @@
+insert into users values(420, 'theking', 'd07ee7e529af02ace472e74ef4be1bd92f3604f6c3a5b11602aad4496161ecb3',  2);
+insert into userinfo values(420, 'Silver', 'Tiik', 'mikszmahalb@gmail.com', 34);
+insert into newsarticles VALUES (900,'http://pervasivegames.files.wordpress.com/2009/08/bruno131.jpg', 'See on hea mees', 'Mina olen Silver Tiik ja soovin teile rääkida mehest, kes mulle meeldib. Ta on pildil.', 5 ,420,1);
+insert into tags VALUES(1,420,900,'Armasz');
+insert into tags VALUES(2,420,900,'Kuum');
+insert into tags VALUES(3,420,900,'hea');
+insert into tags VALUES(4,42,900,'MikszNIIGEI');
