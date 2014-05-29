b8756487d496636ed81b65b4bec58eac3b358d86
HansP <soulyyy@gmail.com>
2014-05-30 01:42:42 +0300

    check

--- src/main/webapp/js/tinymce.js
+++ src/main/webapp/js/tinymce.js
@@ -5,7 +5,7 @@ $(this).ready(tinymce.init({
         "advlist autolink lists link image charmap print preview hr anchor pagebreak",
         "searchreplace wordcount visualblocks visualchars code fullscreen",
         "insertdatetime media nonbreaking save table contextmenu directionality",
-        "emoticons template paste textcolor moxiemanager"
+        "emoticons template paste textcolor"
     ],
     toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
     toolbar2: "print preview media | forecolor backcolor emoticons",
